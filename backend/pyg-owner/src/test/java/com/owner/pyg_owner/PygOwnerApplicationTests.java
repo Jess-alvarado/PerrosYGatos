c5f97216801785624e40e8af13173b4a4b8bac3d
Jess-alvarado <jessica.alvarado.mensajes@gmail.com>
2025-10-27 18:05:06 -0300

    feat(owner): add initial structure for owner microservice

---
+++ backend/pyg-owner/src/test/java/com/owner/pyg_owner/PygOwnerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.owner.pyg_owner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PygOwnerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
