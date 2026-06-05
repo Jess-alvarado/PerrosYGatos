d6047565826892397e1aa016d3c92c73378f1653
Jessica Alvarado Cuevas <96460822+Jess-alvarado@users.noreply.github.com>
2026-06-05 01:22:41 -0400

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,6 +5,8 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
-    allowedHosts: 'all'
+    allowedHosts: [
+      'harddisk-handshake-decode.ngrok-free.dev'
+    ]
   }
 })
