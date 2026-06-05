74da36924f7e37534c7f0256b743091c55b6c0fb
Jess-alvarado <jessica.alvarado.mensajes@gmail.com>
2026-06-05 17:52:41 -0400

    test: configure application-test.yml and enable pipeline unit tests

--- backend/pyg-professional/src/test/java/com/professional/pyg_professional/PygProfessionalApplicationTests.java
+++ backend/pyg-professional/src/test/java/com/professional/pyg_professional/PygProfessionalApplicationTests.java
@@ -1,9 +1,11 @@
 package com.professional.pyg_professional;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class PygProfessionalApplicationTests {
 
 	@Test
